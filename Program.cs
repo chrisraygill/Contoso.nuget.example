f0805007ead5878800b06d9cf8cb6df002e880df
Christopher R. Gill <christopher.gill@microsoft.com>
2020-03-12 11:44:07 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contoso.nuget.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
